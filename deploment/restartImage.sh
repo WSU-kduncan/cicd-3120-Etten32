a08b1bf1e7adda0cab2c2d64809b470d50b40620
Robert Green <etten327vcr@gmail.com>
2022-04-19 18:39:53 -0400

    restart script working

--- deploment/restartImage.sh
+++ deploment/restartImage.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# stop/remove current image
-docker rm 
+# stop/remove current container
+docker stop activeWebserv
+#delete docker image
+docker image rm ette32/firstrepo:latest
 # pull new image
+docker pull etten32/firstrepo:latest
 # run new image in container
+docker run -d --rm --name activeWebserv -p 80:80 etten32/firstrepo:latest
 
